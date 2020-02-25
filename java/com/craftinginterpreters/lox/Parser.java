07eabff8fce21e92e533b969a18d4d58d8e1c5cd
Ben Stephens <ben.stephens.programmer@gmail.com>
2020-02-25 16:35:16 -0500

    Add function declarations

--- java/com/craftinginterpreters/lox/Parser.java
+++ java/com/craftinginterpreters/lox/Parser.java
@@ -31,6 +31,7 @@ class Parser {
 
     private Stmt declaration() {
 	try {
+           if (match(FUN)) return function("function");
 	    if (match(VAR)) return varDeclaration();
 
 	    return statement();
@@ -147,6 +148,26 @@ class Parser {
 
 	consume(RIGHT_BRACE, "EXPECT '}' after block.");
 	return statements;
+    }
+
+    private Stmt.Function function(String kind) {
+        Token name = consume(IDENTIFIER, "Expect " + kind + " name.");
+        consume(LEFT_PAREN, "Expect '(' after " + kind + " name.");
+        List<Token> parameters = new ArrayList<>();
+        if (!check(RIGHT_PAREN)) {
+            do {
+                if (parameters.size() >= 255) {
+                    error(peek(), "Cannot have more than 255 parameters.");
+                }
+
+                parameters.add(consume(IDENTIFIER, "Expect parameter name."));
+            } while (match(COMMA));
+        }
+        consume(RIGHT_PAREN, "Expect ')' after parameters.");
+
+        consume(LEFT_BRACE, "Expect '{' before " + kind + " body.");
+        List<Stmt> body = block();
+        return new Stmt.Function(name, parameters, body);
     }
 
     private Expr assignment() {
