420d13d3c670738dc0816fb1a6bea513a932e2d3
Ben Stephens <ben.stephens.programmer@gmail.com>
2020-02-26 00:11:42 -0500

    Force AstPrinter to (poorly) support call expressions

--- java/com/craftinginterpreters/lox/AstPrinter.java
+++ java/com/craftinginterpreters/lox/AstPrinter.java
@@ -42,6 +42,11 @@ class AstPrinter implements Expr.Visitor<String> {
         return parenthesize(expr.operator.lexeme, expr.right);
     }
 
+    @Override
+    public String visitCallExpr(Expr.Call expr) {
+	return "CALL";
+    }
+
     private String parenthesize(String name, Expr... exprs){
         StringBuilder builder = new StringBuilder();
 
