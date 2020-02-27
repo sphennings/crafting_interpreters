62b22778927cf89164092e7ea0e4b1a50102a28d
Ben Stephens <ben.stephens.programmer@gmail.com>
2020-02-27 13:58:22 -0500

    Add LoxClass objects

--- java/com/craftinginterpreters/lox/Parser.java
+++ java/com/craftinginterpreters/lox/Parser.java
@@ -31,7 +31,8 @@ class Parser {
 
     private Stmt declaration() {
 	try {
-           if (match(FUN)) return function("function");
+            if (match(CLASS)) return classDeclaration();
+            if (match(FUN)) return function("function");
 	    if (match(VAR)) return varDeclaration();
 
 	    return statement();
@@ -39,6 +40,19 @@ class Parser {
 	    synchronize();
 	    return null;
 	}
+    }
+
+    private Stmt classDeclaration() {
+        Token name = consume(IDENTIFIER, "Expect class name.");
+        consume(LEFT_BRACE, "Expect '{' before class body.");
+
+        List<Stmt.Function> methods = new ArrayList<>();
+        while (!check(RIGHT_BRACE) && !isAtEnd()) {
+            methods.add(function("method"));
+        }
+
+        consume(RIGHT_BRACE, "Expect '}' after class body.");
+        return new Stmt.Class(name, methods);
     }
 
     private Stmt statement() {
