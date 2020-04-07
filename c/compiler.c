ab4d802a5c2696a467885c22edb8f47425b8310f
Ben Stephens <ben.stephens.programmer@gmail.com>
2020-04-06 22:43:26 -0400

    Support expression statements

--- c/compiler.c
+++ c/compiler.c
@@ -277,6 +277,12 @@ static void expression() {
   parsePrecedence(PREC_ASSIGNMENT);
 }
 
+static void expressionStatement() {
+  expression();
+  consume(TOKEN_SEMICOLON, "Expect ';' after expression.");
+  emitByte(OP_POP);
+}
+
 static void printStatement() {
   expression();
   consume(TOKEN_SEMICOLON, "Expect ';' after value.");
@@ -290,6 +296,8 @@ static void declaration() {
 static void statement() {
   if (match(TOKEN_PRINT)) {
     printStatement();
+  } else {
+    expressionStatement();
   }
 }
 
