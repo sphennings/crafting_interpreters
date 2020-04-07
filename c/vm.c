31143de609561acd5c8907b1d5caf84ad7fb428b
Ben Stephens <ben.stephens.programmer@gmail.com>
2020-04-07 00:29:31 -0400

    Allow variable assignment

--- c/vm.c
+++ c/vm.c
@@ -126,6 +126,16 @@ static InterpretResult run() {
         break;
       }
 
+      case OP_SET_GLOBAL: {
+        ObjString* name = READ_STRING();
+        if (tableSet(&vm.globals, name, peek(0))) {
+          tableDelete(&vm.globals, name);
+          runtimeError("Undefined variable '%s'.", name->chars);
+          return INTERPRET_RUNTIME_ERROR;
+        }
+        break;
+      }
+
       case OP_DEFINE_GLOBAL: {
         ObjString* name = READ_STRING();
         tableSet(&vm.globals, name, peek(0));
