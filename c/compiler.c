9fe55bf933121c32f23f2632aa6fd8040305fa57
Ben Stephens <ben.stephens.programmer@gmail.com>
2020-04-09 15:41:35 -0400

    Support or statements

--- c/compiler.c
+++ c/compiler.c
@@ -328,6 +328,17 @@ static void number(bool canAssign) {
   emitConstant(NUMBER_VAL(value));
 }
 
+static void or_(bool canAssign) {
+  int elseJump = emitJump(OP_JUMP_IF_FALSE);
+  int endJump = emitJump(OP_JUMP);
+
+  patchJump(elseJump);
+  emitByte(OP_POP);
+
+  parsePrecedence(PREC_OR);
+  patchJump(endJump);
+}
+
 static void string(bool canAssign) {
   emitConstant(OBJ_VAL(copyString(parser.previous.start + 1,
                                   parser.previous.length - 2)));
@@ -403,7 +414,7 @@ ParseRule rules [] = {
   { NULL,     NULL,    PREC_NONE },        // TOKEN_FUN
   { NULL,     NULL,    PREC_NONE },        // TOKEN_IF
   { literal,  NULL,    PREC_NONE },        // TOKEN_NIL
-  { NULL,     NULL,    PREC_NONE },        // TOKEN_OR
+  { NULL,     or_,     PREC_OR },          // TOKEN_OR
   { NULL,     NULL,    PREC_NONE },        // TOKEN_PRINT
   { NULL,     NULL,    PREC_NONE },        // TOKEN_RETURN
   { NULL,     NULL,    PREC_NONE },        // TOKEN_SUPER
